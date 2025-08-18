ff0a6737daec5705db84e98918f1db8c52c548c3
Wallace Kelly <wallace.kelly@digitalasset.com>
2025-08-18 18:06:27 -0400

    Add proxy to vite.config.js

--- react-ledger-api/vite.config.ts
+++ react-ledger-api/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy requests from /docs to your API server
+      '/api/json-api': {
+        target: 'http://localhost:7575',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/json-api/, ''),
+      },
+    },
+  },
 })
